ec6d5400330874b6f861c084f760e13a25147809
elizarkondrasov <charleyvince@mail.ru>
2019-09-19 11:37:11 +0300

    Initial Commit

---
+++ 2ndPhoneNumber/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  2ndPhoneNumber
+//
+//  Created by Елизар Кондрашов on 19/09/2019.
+//  Copyright © 2019 Елизар Кондрашов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
